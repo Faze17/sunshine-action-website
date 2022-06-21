2d8996ca8b0345b18c409085961c6c9c5dde95f6
Faze17 <86742998+Faze17@users.noreply.github.com>
2022-06-21 23:20:09 +0800

    Updates

---
+++ precache-manifest.8769050255ccb638682844f8d4db9ace.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a4019fb23e7f2ae6fd7",
+    "url": "/sunshine-website-f/static/css/main.a4e4719b.chunk.css"
+  },
+  {
+    "revision": "0a4019fb23e7f2ae6fd7",
+    "url": "/sunshine-website-f/static/js/main.0a4019fb.chunk.js"
+  },
+  {
+    "revision": "bc5aa240e4f8d549a981",
+    "url": "/sunshine-website-f/static/css/1.7b6f9b5f.chunk.css"
+  },
+  {
+    "revision": "bc5aa240e4f8d549a981",
+    "url": "/sunshine-website-f/static/js/1.bc5aa240.chunk.js"
+  },
+  {
+    "revision": "660f7ba4845e582d6228",
+    "url": "/sunshine-website-f/static/js/runtime~main.660f7ba4.js"
+  },
+  {
+    "revision": "83c1db5eacd0a29232c2fe52101357c8",
+    "url": "/sunshine-website-f/index.html"
+  }
+];
