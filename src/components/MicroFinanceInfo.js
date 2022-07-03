ce0ffccc494e30b3bc4baecd9118a195ae5bb605
prakhars03 <107943143+prakhars03@users.noreply.github.com>
2022-07-04 00:41:58 +0530

    03/07/22 change

---
+++ src/components/MicroFinanceInfo.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './HomePageInfo.css';
+
+
+function MicroFinanceInfo() {
+    return (
+        <div className="homepageinfo" style={{background: 'rgb(82,97,147'}}>
+   <div className="container">
+      <div className="row">
+         <div className="col-md-2"></div>
+
+         <div className="col-md-8">
+            <div className="container">
+               <div id="ourstory" className="p-4 text-dark rounded-3">
+                  <h1>Sunshine MicroFinance</h1> </div>
+
+        <br />
+
+<div className="row">
+    <div>
+    <p>MicroFinance helps small entrepreneurs and businesses, that lack access to normal commercial banking due to high costs, succeed by helping them financially. Sunshine Action started its MicrroFinance program in Vietnam in 2014, when it granted 2 groups of farmers and laborers a few cows and micro-loans to help them pursue their projects.</p>
+
+
+
+    <p>Sunshine Action is NOT subvented by the Hong Kong government or any government, the Community Chest of Hong Kong, or the Lottery Fund of the Jockey Club of Hong Kong. All the funding comes from the general public and corporations, and on average 98% of the food, materials and contributions from Sunshine Action are purchased.</p>
+
+
+
+    </div>
+
+</div>
+
+<div className="col-md-1"></div>
+
+</div>
+
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"> </script>
+
+
+</div>
+</div>
+    </div>
+
+
+    );
+}
+
+export default MicroFinanceInfo;
