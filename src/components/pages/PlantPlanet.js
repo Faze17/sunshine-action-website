8be54ea78b9e821d6e967fe4ad6902090a674086
Rachel Kovach-Fuentes <rkovachfuentes@uchicago.edu>
2022-07-29 13:30:42 -0400

    reformatted plant the planet, chd

--- src/components/pages/PlantPlanet.js
+++ src/components/pages/PlantPlanet.js
@@ -19,11 +19,11 @@ function PlantPlanet() {
                 <li>To date, Sunshine Action has planted over 322,000 trees, plants, fruit trees and flowers in Hong Kong, Kenya, China, Sri Lanka, Mongolia, Thailand, Nepal, and India.</li>
                 <li>Newly planted trees are sponsored by local farmers and schools. Sunshine Action plants new trees/plants on all charity trips outside of Hong Kong.</li>
                 <li><b>In 2022, Sunshine Action is struggling to financially sustain all programs, including the Plant the Planet Program. Your contribution would mean a lot to us. <a href="/donate">Learn how to donate here.</a></b></li>
-                <img src="../plant-the-planet-1.jpg" alt="Plant the Planet 1"></img>
+                <ImageComp src="plant-the-planet-1.jpg" />
                 <BigHeader text = "History of Plant the Planet" />
                 <p>Plant the Planet began in 2010 as a way of fighting climate change on Sunshine Action charity trips abroad. New trees were planted on charity trips to countries outside of Hong Kong. Approximately 280,000 of these trees are in Mongolia and China.</p>
                 <p>Due to COVID-19 travel restrictions, Plant the Planet has been suspended since the beginning of the pandemic. However, once travel restrictions are lifted, the program will resume normal activities.</p>
-                <img src="../images/plant-the-planet-2.jpg" alt="Plant the Planet 2"></img> 
+                <ImageComp src="plant-the-planet-2.jpg" />
                 <BigHeader text = "Why is Plant the Planet Important?" />
                 <p>Did you know that an area of rainforest the size of a football field is being destroyed every second? Tropical forests cover 23% of the Earth’s land surface but are disappearing at a rate of 4.6 million hectares per year. Trees are crucial to absorbing carbon dioxide and producing the oxygen that helps sustain humanity. They reduced the greenhouse effect by shading our homes and office buildings, and help maintain the environment by preventing soil erosion, maintaining local rainfall patterns, and maintaining populations of plants, animals, and insects.</p>
                 <p>Planting trees is one of the cheapest and most effective ways of drawing excess carbon dioxide out of the atmosphere. </p>
@@ -32,8 +32,8 @@ function PlantPlanet() {
                 <li>On average, each American uses more than 600 pounds of paper and almost 200 board feet of timber per year</li>
                 <li>A single mature tree can absorb carbon dioxide at a rate of 22.7 kg/year (48 lbs) and release enough oxygen back into the atmosphere to support 2 human beings.</li>
                 <li>One acre of trees annually consumes the amount of carbon dioxide equivalent to that produced by driving an average car for 41,483km (26,000 miles). That same acre of trees also produces enough oxygen for 18 people to breathe for a year.</li>
-                <img src="../plant-the-planet-3.jpg" alt="Plant the Planet 3"></img>
-                <img src="../plant-the-planet-4.jpg" alt="Plant the Planet 4"></img>
+                <ImageComp src="plant-the-planet-3.jpg" />
+                <ImageComp src="plant-the-planet-4.jpg" alt="Plant the Planet 4" />
             </div>
         </div>       
         <Footer />
