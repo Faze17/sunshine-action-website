384980ac76b9cc05f18f6cc53e97656d556a2ada
ql420 <94789931+ql420@users.noreply.github.com>
2022-07-29 21:57:30 +0100

    FortuneBagUK

--- src/components/pages/ProjectPages.js
+++ src/components/pages/ProjectPages.js
@@ -2,14 +2,17 @@ import INDAPage from "./projects/INDA.js"
 import myprojectPage from "./projects/myproject.js"
 import publichealthPage from "./projects/publichealth.js"
 import usfortunebagsPage from "./projects/usfortunebags.js"
+import ukfortunebagsPage from "./projects/ukfortunebags.js"
 
 export {
 projectPageInfo,
 INDAPage,
 myprojectPage,
 publichealthPage,
-usfortunebagsPage
+usfortunebagsPage,
+ukfortunebagsPage
 }
 
 //PROJECTPAGEINFO
-const projectPageInfo = {"INDAPage":["INDA","INDIA",1,"india2017.jpeg"],"myprojectPage":["myproject","My Project",1,"1.jpeg"],"publichealthPage":["publichealth","Public Health Project",2,"Examination_of_world's_health_-_journal.pmed.0010003.g001.png"],"usfortunebagsPage":["usfortunebags","Fortune Bags",3,"image005.jpg"]}
+//1 is HK Page, 2 is UK Page, 3 is US Page
+const projectPageInfo = {"INDAPage":["INDA","INDIA",1,"india2017.jpeg"],"myprojectPage":["myproject","My Project",1,"1.jpeg"],"publichealthPage":["publichealth","Public Health Project",2,"Examination_of_world's_health_-_journal.pmed.0010003.g001.png"],"ukfortunebagsPage":["ukfortunebags","Fortune Bags",2,"fortunebaguk.jpeg"],"usfortunebagsPage":["usfortunebags","Fortune Bags",3,"image005.jpg"]}
