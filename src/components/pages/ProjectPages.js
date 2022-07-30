c6629f4f515fff72eb342c50422324074974a97c
Jack Rong <rongjackmiles@gmail.com>
2022-07-30 11:57:42 +0800

    everything good

--- src/components/pages/ProjectPages.js
+++ src/components/pages/ProjectPages.js
@@ -14,5 +14,4 @@ ukfortunebagsPage
 }
 
 //PROJECTPAGEINFO
-//1 is HK Page, 2 is UK Page, 3 is US Page
 const projectPageInfo = {"INDAPage":["INDA","INDIA",1,"india2017.jpeg"],"myprojectPage":["myproject","My Project",1,"1.jpeg"],"publichealthPage":["publichealth","Public Health Project",2,"Examination_of_world's_health_-_journal.pmed.0010003.g001.png"],"ukfortunebagsPage":["ukfortunebags","Fortune Bags",2,"fortunebaguk.jpeg"],"usfortunebagsPage":["usfortunebags","Fortune Bags",3,"image005.jpg"]}
