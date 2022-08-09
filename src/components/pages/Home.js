f4f74a30ea9c29dcc77261914e33d3964e2203fe
ql420 <94789931+ql420@users.noreply.github.com>
2022-08-09 15:56:09 +0100

    herovideo and performance report mobile fix

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -29,9 +29,6 @@ function Home(props) {
     <>
       <HeroHome name = "FEED THE HUNGRY AND WARM THE ABANDONED" selfintro = "We are a charity which aims to help people facing poverty and homelessness." subtitle = "Sunshine Action has made a long-term commitment to help those in need on achieve lasting growth and success in the coming years, and therefore funding is crucial for the future."/>
       <HomePageInfo />
-      <Link to="/reports" className = "reportsBanner">
-        SUNSHINE ACTION PERFORMANCE REPORTS
-      </Link>
       {renderProjects()}
       {/* <Cards /> */}
       <Footer />
