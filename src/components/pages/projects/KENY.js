96be7088a01fa9965e0543c158fc9324e8c6c61d
vanessa <vanessachanks@gmail.com>
2023-01-19 12:41:00 +0800

    pull request

--- src/components/pages/projects/KENY.js
+++ src/components/pages/projects/KENY.js
@@ -18,8 +18,34 @@ export default KENYPage;
 function KENYPage() {
 return (
 <>
-      <HeroComp name = "COMING SOON" />
-      <Footer />
+<HeroComp name = "KENYA" />
+<HomePageLogo />
+<div className="projectBanner" style={{background: 'rgb(82,97,147'}}>
+<div className="container" id = "BEGINREADHERE">
+
+<BigHeader text = "Kenya Volunteer Tour" />
+<SmallHeader text = "Sept 2010" />
+<ImageComp src = "26374CD6-96D9-44BC-AC0F-A2BA5A6BC8A2-resized.jpeg.png" />
+<p>Two Sunshine Action volunteers visited Nairobi Kenya and the surrounding area and donated over 6'500 kgs food & material, covering approx. 750 families cases & children. </p>
+
+<BigHeader text = "Plant the Planet" />
+<ImageComp src = "AC4D2F70-22D7-4F24-9C71-21D711A45C4E-resized.jpeg.png" />
+<p>In SVD secondary school in Katani & Kahuho Primary School planted several fruit trees, including  Orange, Avocado, Limon, Mangos, Pawpaws, Guavas, Wild Apples, etc. </p>
+
+<BigHeader text = "Henry Wanyoike Foundation" />
+<SmallHeader text = "March 2005" />
+<ImageComp src = "87A75A5E-C2C7-4239-8BAB-EABB92F81546-resized.jpeg.png" />
+<p>This foundation is a registered organization by the government of Kenya through registry of societies, registration no. 24475 on the 29th.</p>
+<p>The Henry Wanyoike Foundation is named after Henry Wanyoike the visually impaired athlete from Kenya. Wanyoike is a world record holder of the 5,000m at the Sydney Paralympics in 2000, a Gold in the 10,000m Paralympics of Athens 2004 and most recently a bronze in the 5000m Beijing Paralympics of 2008. Along side his partner, Joseph Kibunja, Sunshine Action works on the first program in Kikuyu area. Besides his athletic career, Henry Wanyoike has always given something back to the community. He has participated in many charitable activities in Kenya particularly to assist the poor, persons with disabilities, orphans and other less privileged people. He contributes through giving donations, participating in charitable events, initiating community service projects, partnering with other organizations.</p>
+
+<BigHeader text = "Climb Mount Longonot" />
+<SmallHeader text = "2010" />
+<ImageComp src = "54CD6E02-F755-417C-898A-63B60B8C1A9F-resized.jpeg.png" />
+<p>This Fund Raising Event is for Kenya Society for the Blind which is a charitable organization established in 1956 by an Act of Parliament of Kenya. KSB is a National Organization charged with the responsibility of serving all citizens of Kenya who are visually impaired or are at a risk of going blind. The Kenya Society for the Blind reaches out to visually impaired people in communities and helps to create a conducive environment for the empowerment of the visually impaired by promoting their welfare, training, education and employment, so that they may become self-sufficient and self-reliant.</p>
+
+</div>
+</div>
+<Footer />
 </>
 );
 };
--- src/components/pages/projects/VIET.js
+++ src/components/pages/projects/VIET.js
@@ -20,19 +20,36 @@ return (
 <>
 <HeroComp name = "VIETNAM" />
 <HomePageLogo />
+
 <div className="projectBanner" style={{background: 'rgb(82,97,147'}}>
 <div className="container" id = "BEGINREADHERE">
 
-<BigHeader text = "2016 Charity Tour" />
+<BigHeader text = "Sunshine Musical Charity Evening" />
+<SmallHeader text = "June 2017" />
+<ImageComp src = "IMG_1688.PNG" />
+<p>Since 2009, Sunshine Action has been supporting many projects in Vietnam. One of the orphanages, the “Gia dinh Tinh Thuong Te-Phan” orphanage is a very small center taking care mostly mentally & handicapped orphans (69) in the city of Ho Chi Minh. Established in 1985 by Brother Hoàng Văn Bình, “Te-Phan Loving Family Orphanage” has more than 31 years of experience in caring for the disadvantaged people.One of the young performer, a completely blind pianist NHI will play piano on the following evening together with local artists to raise fund in helping the treatments and studies of the orphans </p>
+<p>Nhi was born 8 Dec, 2004, left at hospital and was born prematurely and so weak. By the time volunteers from Sunshine Action reached there, it was too late and she lost her sight already. She started to play piano around grade 4 when we were given an old piano. She although is an epilepsy and take medicine everyday but she is talented at musical. With the support from Sunshine Action to study musical, she started to teach others kids in the orphanage like Xuan, An and Nghia to play piano too. In her school, they recognized her talented then chose her for musical’s team of her school. </p>
+
+<BigHeader text = "Charity Tour" />
 <SmallHeader text = "January 2016" />
-<p> The mission of this trip was food distribution to the poorest of the poor. Volunteers visited and volunteered for several local organizations, and provided Sunshine Student Sponsorships to poor children.  This program was founded in 2013, and 30 children are sponsored on a yearly basis. Volunteers also visted long-term supported cases of HIV/AIDs and others with disabilities or chronic illness, and evaluated the Microfinance program in Vietnam.</p>
-
-<BigHeader text = "2014 Charity Tour" />
-<SmallHeader text = "January 2014" />
+<ImageComp src = "18112408-CA13-475F-AE34-28AFB84FECB2_resized.jpeg.png" />
 <p>The mission of this trip was food distribution to the poorest of the poor. Volunteers visited and volunteered for several local organizations, and provided Sunshine Student Sponsorships to poor children.  This program was founded in 2013, and 30 children are sponsored on a yearly basis. Volunteers also visted long-term supported cases of HIV/AIDs and others with disabilities or chronic illness, and evaluated the Microfinance program in Vietnam.</p>
-<BigHeader text = "Other Efforts" />
-<p>Over the years, Sunshine action has provided water pumps, built toilets, and distributed food, medicine and other supplies to those in need in Vietnam. On each Charity trip, volunteers planted trees as part of the Plant the Planet global project.</p>
 
+<BigHeader text = "Sunshine Student Sponsership" />
+<ImageComp src = "DCD96E95-06C7-4FF8-878D-FB7A98A06C34-resized.jpeg.png" />
+<p>This program started in Vietnam in 2013, sponsoring 30 childen on yearly basis and this is the first time we meet them personally in the villages </p>
+
+<BigHeader text = "Sunshine Microfinance Program" />
+<ImageComp src = "4B478A32-DB7A-45FD-B349-4EC99DD0BFC6_resized.jpeg.png" />
+<p>Sunshine Action started this program in Vietnam in 2014 and started by meeting 2 groups of mainly farmers and labor workers. Each group consisting of 6 people and after assessment and interview, Sunshine Action granted few cows and micro-loans to the group in pursing several projects.</p>
+
+<BigHeader text = "Other Actions" />
+<SmallHeader text = "Distribution of materials" />
+<ImageComp src = "22211426-4270-4BBC-9C0D-117E5F75B7EE_resized.jpeg.png" />
+<p>Following Sunshine Action's 6 years of tradition in distributing rice, packed food, noodles, cooking sauces for over 350 poor families, estimated to benefit some 1,500 people directly.Our target groups are from extremely poor families, including elderly, mentally & physically handicapped, severely sick patients, etc.</p>
+<SmallHeader text = "Free Medicine" />
+<ImageComp src = "4B478A32-DB7A-45FD-B349-4EC99DD0BFC6_resized.jpeg.png" />
+<p>Sunshine Action has been sponsoring free medicine for over 3 years such as giving out free medicine and herbal to the poor families in the villages. Sunshine Action has been sponsoring this program for 3 years.</p>
 </div>
 </div>
 <Footer />
